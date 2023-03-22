96b7b78993068d9f87a68781389922d8213f0c2c
pabs3745 <pablo3745@gmail.com>
2023-03-22 10:32:22 +0100

    Added Scroll functionality to make it look better in small screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import CardList from "./CardList";
-import {robots} from './robots';
 import SearchBox from "./SearchBox";
 import './App.css';
+import Scroll from "./Scroll";
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }))
+        
     }
 
     onSearchChange = (event) => {
@@ -29,7 +36,10 @@ class App extends React.Component {
             <div className="tc">
                 <h1 className="f1">Robot Friends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filterRobot}/>
+                <Scroll>
+                    <CardList robots={filterRobot}/>
+                </Scroll>
+                
             </div>
             
         )
